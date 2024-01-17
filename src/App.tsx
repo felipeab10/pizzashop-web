88b5cc64247699952decb4a5029c442a50b47a5e
felipe almeida batista <felipeab10@hotmail.com>
2024-01-17 10:59:51 -0400

    Adicionado gestao de tema dark light,menu de opcões de conta

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,19 @@ import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
 
+import { ThemeProvider } from './components/theme/theme-provider'
 import { router } from './routes'
 
 export function App() {
   return (
     <HelmetProvider>
-      <Toaster richColors />
+      <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
+        <Toaster richColors />
 
-      <Helmet titleTemplate="%s | pizza.shop" />
+        <Helmet titleTemplate="%s | pizza.shop" />
 
-      <RouterProvider router={router} />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   )
 }
